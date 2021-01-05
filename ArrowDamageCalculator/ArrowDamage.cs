8ad359d4388a5f34686969fc8af28c21236d78de
Swdenis <swdenis@mail.ru>
2021-01-05 16:57:33 +0100

    * .gitignore: Initial commit
    * ArrowDamageCalculator.sln:
    * Program.cs:
    * ArrowDamage.cs:
    * SwordDamage.cs:
    * ArrowDamageCalculator.csproj:

---
+++ ArrowDamageCalculator/ArrowDamage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace ArrowDamageCalculator
+
+
+{
+    public class ArrowDamage
+    {
+        public ArrowDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+
+        public const decimal BASE_MULTIPLIER = 0.35M;
+        public const decimal MAGIC_MULTIPLIER = 2.5M;
+        public const decimal FLAME_DAMAGE = 1.25M;
+
+        public int Damage { get; private set; }
+
+        private bool magic;
+
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+
+        private bool flaming;
+
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        private int roll;
+
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        public void CalculateDamage()
+        {
+            decimal baseDamage = Roll * BASE_MULTIPLIER;
+            if (Magic) baseDamage *= MAGIC_MULTIPLIER;
+            if (Flaming) Damage = (int)Math.Ceiling(baseDamage + FLAME_DAMAGE);
+            else Damage = (int)Math.Ceiling(baseDamage);
+        }
+
+    } 
+    
+}
---
+++ ArrowDamageCalculator/SwordDamage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+namespace ArrowDamageCalculator
+{
+    public class SwordDamage
+    {
+        public SwordDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+        public int Damage { get; private set; }
+
+        private int roll;
+
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+
+
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+
+        }
+
+        private bool magic;
+
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+
+        private bool flaming;
+
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+    }
+}
